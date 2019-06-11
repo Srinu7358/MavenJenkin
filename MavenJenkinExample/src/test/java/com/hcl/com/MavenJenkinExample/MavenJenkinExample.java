c1dcbc459a7174e857b050142e229825ef3c89eb
SRINU <srinuvasarao7358@gmail.com>
2019-06-11 18:34:22 +0530

    All Latest kids mens committed

--- MavenJenkinExample/src/test/java/com/hcl/com/MavenJenkinExample/MavenJenkinExample.java
+++ MavenJenkinExample/src/test/java/com/hcl/com/MavenJenkinExample/MavenJenkinExample.java
@@ -18,7 +18,7 @@ public class MavenJenkinExample {
 			
 			//driver.findElement(By.xpath("//A[@name='&lpos=nav_topnav>home>>sale>'][text()='sale']")).click();
 			//driver.findElement(By.xpath("//A[@name='&lpos=nav_topnav>embedHeader>>kids>'][text()='kids']")).click();
-			driver.findElement(By.xpath("//A[@name='&lpos=nav_topnav>embedHeader>>men>'][text()='Men']")).click();
+			//driver.findElement(By.xpath("//A[@name='&lpos=nav_topnav>embedHeader>>men>'][text()='Men']")).click();
 			driver.findElement(By.xpath("//A[@name='&lpos=nav_topnav>embedHeader>>women>'][text()='Women']")).click();
 			
 			driver.close();
